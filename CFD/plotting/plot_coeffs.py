630a184419f192447c27c7d24077699b03606fee
Christopher M <christopher_mao@branson.org>
2026-01-01 23:24:20 -0700

    add velocityUniformity function

--- CFD/plotting/plot_coeffs.py
+++ CFD/plotting/plot_coeffs.py
@@ -1,19 +1,18 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Define the path to your data file
-# Assuming you run this from the IBF_CFD directory
+# Define path to data file
 data_file = 'IBF_CFD/postProcessing/forceCoeffs1/0.01/coefficient.dat'
 data_file = 'CFD/IBF_CFD/postProcessing/forceCoeffs1/0.01/coefficient.dat'
 
-# Read the data, skipping the non-data header lines (#)
+# Read data, skipping the non-data header lines (#)
 # We plot column 1 (Time/Iteration) vs column 2 (Cd) and column 4 (Cl)
 df = pd.read_csv(data_file, sep='\s+', skiprows=1, header=None, comment='#')
 
-# Plot the Drag Coefficient (Cd) and Lift Coefficient (Cl)
+# Plotting
 plt.figure(figsize=(10, 6))
 
-print(df)
+# print(df)
 
 # Column 0 (Time/Iteration) vs Column 1 (Cd)
 plt.plot(df[0], df[1], label='$C_d$ (Drag Coefficient)', color='blue')
